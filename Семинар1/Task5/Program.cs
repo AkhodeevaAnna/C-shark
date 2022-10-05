3a3089c9b22434895e5d66c74a717c31ae9ec9fc
Anna <akhodeeva@yandex.ru>
2022-10-05 19:47:34 +0300

    Семинар 1

---
+++ Семинар1/Task5/Program.cs
@@ -0,0 +1,14 @@
+﻿// 5. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+// 2 -> " -2, -1, 0, 1, 2"
+
+Console.WriteLine("Введите N");
+int N = Convert.ToInt32(Console.ReadLine());
+int MinN = N - N * 2;
+int a = MinN;
+while (MinN <= a && a < N)
+{
+Console.Write($"{a} ");
+a = a + 1;
+}
+Console.Write($"{N} ");
---
+++ Семинар1/Задание1/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int result = num * num;
+// Console.Write("Квадрат числа " + num + " = " + result);
+Console.Write($"Квадрат числа {num} = {result}");
